372dc52e3e4100eee24fe3c2a45dc8bd4ec86a6a
Princi Rakotoarison <princirakotoarison07@gmail.com>
2026-01-19 20:47:35 +0300

    123

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -34,7 +34,7 @@
   <div class="d-flex align-items-center justify-content-between">
     <a href="{{ url('/dashboard') }}" class="logo d-flex align-items-center">
       <img src="{{ asset('assets/img/logo.png') }}" alt="">
-      <span class="d-none d-lg-block">Vaika</span>
+      <span class="d-none d-lg-block">EDL-auto</span>
     </a>
     <i class="bi bi-list toggle-sidebar-btn"></i>
   </div>
